75658b4c5cbe9a5dbd623701c5fa06f241e99db9
Braian Arguello <braian.arguello@hotmail.com>
2022-01-24 23:52:13 -0300

    [ADD] se borro lo innecesario de la base, se crea el componente NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+// import './App.css';
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+          <h1> My e-comerce</h1>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+const NavBar = () =>{
+    return <nav>
+        <ul>
+            <li>Inicio</li>
+            <li>Imagenes</li>
+            <li>Sobre Nosotros</li>
+            <li>Contacto</li>
+        </ul>
+    </nav>
+}
+export default NavBar;
